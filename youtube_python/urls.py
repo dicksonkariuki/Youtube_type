681e73e8e237ae47c3fa23d1af70444e4689b2ae
dicksonkariuki <dicksonkariuki4@gmail.com>
2019-12-19 16:49:25 +0300

    Add project

---
+++ youtube_python/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from youtube.views import HomeView, NewVideo, CommentView, LoginView, RegisterView, VideoView, VideoFileView,LogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view()),
+    path('login', LoginView.as_view()),
+    path('register', RegisterView.as_view()),
+    path('new_video', NewVideo.as_view()),
+    path('video/<int:id>', VideoView.as_view()),
+    path('comment', CommentView.as_view()),
+    path('get_video/<file_name>', VideoFileView.as_view()),
+    path('logout', LogoutView.as_view())
+]
+
+from django.conf import settings
+from django.urls import include, path  
